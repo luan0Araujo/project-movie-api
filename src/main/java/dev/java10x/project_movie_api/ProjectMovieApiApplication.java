a3ed9098aef24330d40641913a23425cc201eeb2
Luan <luanaraujo59@gmail.com>
2025-08-03 16:10:07 -0300

    Spring project start build
    Spring project start build

---
+++ src/main/java/dev/java10x/project_movie_api/ProjectMovieApiApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.project_movie_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMovieApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMovieApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java10x/project_movie_api/ProjectMovieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.project_movie_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMovieApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
